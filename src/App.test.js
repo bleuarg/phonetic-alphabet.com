33b0208da90e73406dc1719b671234fcceaedb25
Patrick Davies <me@patrickdavies.ca>
2019-07-10 15:51:45 -0700

    add test for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const app = mount(<App />);
+  app.unmount();
+});
+
+describe('App', () => {
+  const app = mount(<App />);
+  describe('getPhoneticArrayFromString', () => {
+    it('returns the space string for space character', () => {
+      expect(app.instance().getPhoneticArrayFromString('Abcdefghijklmnopqrstuvwxyz 0123456789')).toEqual(['Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-ray', 'Yankee', 'Zulu ', '(space)', 'Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']);
+    });
+  });
 });
